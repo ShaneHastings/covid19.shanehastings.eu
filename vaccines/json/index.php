2a98fb1e0109946adda72f469f9102481627cbda
Shane Hastings <hastingsshane35@gmail.com>
2021-02-11 22:10:17 +0000

    Added cohort information

--- vaccines/json/index.php
+++ vaccines/json/index.php
@@ -25,6 +25,11 @@ $jsonResponse->populationFullyVaccinated = $populationFullyVaccinated . "%";
 $jsonResponse->vaccineManufacturer->pfizerBioNTech = getGeoHiveVaccineTotalsByManufacturer("pf");
 $jsonResponse->vaccineManufacturer->moderna = getGeoHiveVaccineTotalsByManufacturer("modern");
 $jsonResponse->vaccineManufacturer->astrazeneca = "Data Unavailable";
+
+$jsonResponse->cohorts->Aged65PlusLTRCStaff = getGeoHiveVaccineTotalsByCohort("totalcoh1");
+$jsonResponse->cohorts->FronlineHCW = getGeoHiveVaccineTotalsByCohort("totalcoh2");
+$jsonResponse->cohorts->Others = getGeoHiveVaccineTotalsByCohort("totalcoh16");
+
 //$jsonResponse->dataSource = 'https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/arcgis/rest/services/Covid19_Vaccine_Administration_Data/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=html&token=';
 
 
