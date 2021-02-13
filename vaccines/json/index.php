e6eaba5dfa4aa02d717e5e06cbf0cf642561144c
Shane Hastings <hastingsshane35@gmail.com>
2021-02-13 00:06:31 +0000

    Added cohort dosing responses (e.g. First dose for 65+, and so on)

--- vaccines/json/index.php
+++ vaccines/json/index.php
@@ -24,11 +24,14 @@ $jsonResponse->populationFullyVaccinated = $populationFullyVaccinated . "%";
 
 $jsonResponse->vaccineManufacturer->pfizerBioNTech = getGeoHiveVaccineTotalsByManufacturer("pf");
 $jsonResponse->vaccineManufacturer->moderna = getGeoHiveVaccineTotalsByManufacturer("modern");
-$jsonResponse->vaccineManufacturer->astrazeneca = "Data Unavailable";
+$jsonResponse->vaccineManufacturer->astrazeneca =getGeoHiveVaccineTotalsByManufacturer("az");
 
-$jsonResponse->cohorts->Aged65PlusLTRCStaff = getGeoHiveVaccineTotalsByCohort("totalcoh1");
-$jsonResponse->cohorts->FronlineHCW = getGeoHiveVaccineTotalsByCohort("totalcoh2");
-$jsonResponse->cohorts->Others = getGeoHiveVaccineTotalsByCohort("totalcoh16");
+$jsonResponse->cohorts->Aged65PlusLTRCStaff_1D = getGeoHiveVaccineTotalsByCohort("cohort1");
+$jsonResponse->cohorts->Aged65PlusLTRCStaff_2D = getGeoHiveVaccineTotalsByCohort("cohort1b");
+$jsonResponse->cohorts->FronlineHCW_1D = getGeoHiveVaccineTotalsByCohort("cohort2");
+$jsonResponse->cohorts->FronlineHCW_2D = getGeoHiveVaccineTotalsByCohort("cohort2b");
+$jsonResponse->cohorts->Others_1D = getGeoHiveVaccineTotalsByCohort("othercohort1");
+$jsonResponse->cohorts->Others_2D = getGeoHiveVaccineTotalsByCohort("othercohort2");
 
 //$jsonResponse->dataSource = 'https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/arcgis/rest/services/Covid19_Vaccine_Administration_Data/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=html&token=';
 
